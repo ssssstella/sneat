1297d8366f4c1ec316fc7ee920c13196c8f0a7ea
ssssstella <wt.su77@gmail.com>
2024-01-14 21:55:15 -0800

    remove file

--- src/components/dashboard/CRM.jsx
+++ src/components/dashboard/CRM.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Grid, Paper, Divider, Avatar } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import DrawTable from './DrawTable';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2.5, 3),
+  color: theme.palette.text.secondary,
+}));
 
 export default function CRM() {
   return (
     <div className='crm container'>
-      this is CRM dashboard
+      <Grid item xs={12} className=''>
+          <Item className=''>
+            <div className=''>
+              <h4>Congratulations</h4>
+              <p>You have done 72% more sales today. Check your new badge in your profile.</p>
+              <button>VIEW BADGES</button>
+            </div>
+             </Item>
+        </Grid>
     </div>
   )
 }
--- src/components/dashboard/ECommerce.jsx
+++ src/components/dashboard/ECommerce.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Grid, Paper, Divider, Avatar } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2.5, 3),
+  color: theme.palette.text.secondary,
+}));
 
 export default function ECommerce() {
   return (
     <div className='ecommerce container'>
-      this is eCommerce dashboard
+      <Grid item xs={12} className=''>
+          <Item className=''>
+            <div className=''>
+              <h4>Congratulations</h4>
+              <p>You have done 72% more sales today. Check your new badge in your profile.</p>
+              <button>VIEW BADGES</button>
+            </div>
+             </Item>
+        </Grid>
     </div>
   )
 }
